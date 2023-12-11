414c1c615978a51c00238823c19c852af8ee9bd3
peter <pngugi.wainaina@gmail.com>
2023-12-11 21:52:39 +0300

    assignment done

---
+++ src/components/Article.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Article({ title, date = "January 1, 1970", preview}){
+
+    return(
+       <article>
+        <h3>{title}</h3>
+        <small>
+            {date}min read
+        </small>
+        <p>{preview}</p>
+       </article>
+    );
+}
+export default Article;
